f68f9d73626257c6ba5f0002358fc988d10c130e
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2017-01-09 12:27:41 -0500

    Pleasant Hill 1.2  /site.js

---
+++ site.js
@@ -0,0 +1,8 @@
+var site_json = {
+                    "name" : "The Shops at Rossmoor",
+                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/58332abd6e6f646f31040000/image/jpeg/1483982807000/500.jpg",
+                    "time_zone" : "T08:00:00Z",
+                    "instagram" : "1590821057"
+                }
+                
+               
