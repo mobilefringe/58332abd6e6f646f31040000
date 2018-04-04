d2b4d046c1c42e087497adc2b3b4fd68f66489df
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-04-04 14:59:27 -0400

    Pleasant Hill 1.4  /site.js

---
+++ site.js
@@ -0,0 +1,8 @@
+var site_json = {
+                    "name" : "Downtown Pleasant Hill",
+                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/58332abd6e6f646f31040000/image/jpeg/1484182955000/dph_500x500_logo.jpg",
+                    "time_zone" : "T08:00:00Z",
+                    "social_feed" : "//pleasanthill.mallmaverick.com/api/v3/pleasanthill/social.json"
+                }
+                
+               
